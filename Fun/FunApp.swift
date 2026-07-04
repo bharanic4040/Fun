ecea4eb260aa925039fffaeefb77e5e1ad8624b0
bharani chennu <bharanic404@gmail.com>
2026-07-04 17:29:23 +0530

    first commit

---
+++ Fun/FunApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunApp.swift
+//  Fun
+//
+//  Created by lavanya chennu on 04/07/26.
+//
+
+import SwiftUI
+
+@main
+struct FunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
